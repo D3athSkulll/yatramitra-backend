2d6ac49f0a57ac2ee194114c163cc9aa1eb80f33
udit2303 <udit2303@gmail.com>
2024-08-18 12:59:23 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const api = require("./routes/api");
 const auth = require("./routes/auth");
 const payment = require("./routes/payment");
 const { isLoggedIn } = require("./middleware/login");
-
+const port = process.env.PORT || 3000;
 const JWT_SECRET = process.env.SECRET;
 app.use(cookieParser());
 app.use(session({ secret: JWT_SECRET, resave: false, saveUninitialized: true }));
@@ -25,6 +25,6 @@ app.use(isLoggedIn);
 app.use("/api", api);
 app.use("/payment", payment);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
